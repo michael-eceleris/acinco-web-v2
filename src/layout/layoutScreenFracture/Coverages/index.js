8e345ec2343fce1c6ab52b3f8dcb88e3d788feb0
michael-eceleris <michael@eceleris.com>
2020-12-17 10:52:24 -0500

    page-fractura-pantalla

--- src/layout/layoutScreenFracture/Coverages/index.js
+++ src/layout/layoutScreenFracture/Coverages/index.js
@@ -1,36 +1,34 @@
 import React from "react";
-
 const Coverages = () => {
   return (
     <>
-      <section className="pt-5 pb-5">
-        <div className="container p-0">
-          <h2 className="font-weight-light mb-4 text-center-xs">
-            Detalles del plan Protección Total
+      <section className="border-top">
+        <div className="container ">
+          <h2 className="font-weight-light mb-5 text-center">
+            Detalles del plan Fractura de Pantalla
           </h2>
-          <div className="table-responsive">
-            <h3 className="h5">Coberturas</h3>
-            <table className="table table-striped">
+          <div className="table-responsive mx-auto w--700">
+            <table className="table table-hover table-align-middle font-weight-medium text-center">
+              <h3 className="h5 ml-2 text-align-start mb-3">Coberturas</h3>
               <tbody>
                 <tr>
-                  <td className="text-muted w-50">Reparación de Pantalla</td>
-                  <td>Si</td>
+                  <td className="text-align-start">
+                    Reparación de Pantalla
+                    <small className="d-block text-muted">
+                      cubre costos de reparación en la pantalla, no cubre daños
+                      en la tarjeta principal, ni baterías ni daños por humedad
+                      del equipo
+                    </small>
+                  </td>
                 </tr>
                 <tr>
-                  <td className="text-muted w-50">Reclamaciones</td>
-                  <td>1</td>
-                </tr>
-                <tr>
-                  <td className="text-muted w-50">Hurto Calificado</td>
-                  <td>&ndash;</td>
-                </tr>
-                <tr>
-                  <td className="text-muted w-50">Hurto Simple</td>
-                  <td>&ndash;</td>
-                </tr>
-                <tr>
-                  <td className="text-muted w-50">Daño Total</td>
-                  <td>&ndash;</td>
+                  <td className="text-align-start">
+                    Reclamaciones
+                    <small className="d-block text-muted">
+                      Cuenta con una (1) reclmación anual por fractura de
+                      pantalla en caso de accidente.
+                    </small>
+                  </td>
                 </tr>
               </tbody>
             </table>
--- src/layout/layoutScreenFracture/RecoveriesExclutions/index.js
+++ src/layout/layoutScreenFracture/RecoveriesExclutions/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Dollar from "../../../assets/icons/dollar-sign-solid.svg";
 const Recoveries = () => {
   return (
     <>
@@ -7,8 +7,7 @@ const Recoveries = () => {
         <div className="container p-0">
           <div className="row d-flex flex-wrap align-items-center my-7">
             <div className="col-12 col-lg-6 mb-5">
-              <hr className="h--1 bg-primary w--50" />
-              <h2 className="h1 text-primary mb-4">Recobros</h2>
+              <h2 className="h1 text-primary mb-4 text-center">Recobros</h2>
               <p className="lead mb-5">
                 Si te ocurre alguna circunstancia en la que la pantalla del
                 dispositivo se vea afectaba, ten en cuenta esto esta
@@ -18,10 +17,14 @@ const Recoveries = () => {
             <div className="col-12 col-lg-6 mb-5">
               <div className="row gutters-md gutters-xs--xs">
                 <div className="col-6 jarallax">
-                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-primary-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800">
-                    <i className="fas fa-dollar-sign fs--45"></i>
-                    <h3 className="h5 py-3 ">¿Cuánto pagarás?</h3>
-                    <p>
+                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-primary-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800 h-100">
+                    <div className="text-center pt-3 m-0">
+                      <img src={Dollar} height="50" alt="signo_pesos" />
+                    </div>
+                    <h3 className="h5 py-3 mt-3 text-center ">
+                      ¿Cuánto pagarás?
+                    </h3>
+                    <p className="pt-3 text-justify">
                       El seguro te cubre el 70% del valor del arreglo, por lo
                       tanto solo pagas el 30% del mismo.
                     </p>
@@ -29,10 +32,14 @@ const Recoveries = () => {
                 </div>
 
                 <div className="col-6 jarallax">
-                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-success-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800">
-                    <i className="fi fi-emoji-shocked fs--45"></i>
-                    <h3 className="h5 py-3">¿A qué equivale ese porcentaje?</h3>
-                    <p>
+                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-success-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800 h-100">
+                    <div className="text-center m-0 p-0">
+                      <i className="fi fi-emoji-shocked fs--45"></i>
+                    </div>
+                    <h3 className="h5 py-3 text-center">
+                      ¿A qué equivale ese porcentaje?
+                    </h3>
+                    <p className="text-justify">
                       El porcentaje corresponde a los montos cubiertos por la
                       aseguradora, es decir, el recobro será calculado sobre el
                       precio de reparación de la pantalla.
@@ -42,11 +49,13 @@ const Recoveries = () => {
               </div>
             </div>
           </div>
-
+        </div>
+      </section>
+      <section className="pt-2 pb-2 bg-theme-color-custom-purple">
+        <div className="container p-0">
           <div className="row d-flex flex-wrap align-items-center my-7">
             <div className="order-lg-2 col-12 col-lg-6 mb-5">
-              <hr className="h--1 bg-success w--50" />
-              <h2 className="h1 text-success mb-4">Exclusiones</h2>
+              <h2 className="h1 text-success mb-4 text-center">Exclusiones</h2>
               <p className="lead mb-5">
                 Existen algunas exclusiones en las cuales no son aplicables a
                 las coberturas de este plan, ten en cuenta esto antes de
@@ -56,21 +65,27 @@ const Recoveries = () => {
             <div className="order-lg-1 col-12 col-lg-6 mb-5">
               <div className="row gutters-md gutters-xs--xs">
                 <div className="col-6 jarallax">
-                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-primary-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800">
-                    <i className="fi fi-smartphone fs--45"></i>
-                    <h3 className="h5 py-3">Daño al equipo móvil</h3>
-                    <p>
-                      Daño ocasianado intencionalmente por el usuario, y mala
+                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-primary-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800 h-100">
+                    <div className="text-center p-0 m-0">
+                      <i className="fi fi-smartphone fs--45"></i>
+                    </div>
+                    <h3 className="h5 py-3 text-center">
+                      Daño al equipo móvil
+                    </h3>
+                    <p className="text-justify">
+                      Daño ocasionado por el usuario intencionalmente, y mala
                       manipulación en puntos no autorizados.
                     </p>
                   </div>
                 </div>
 
                 <div className="col-6 jarallax">
-                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-success-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800">
-                    <i className="fi fi-round-info-full fs--45"></i>
-                    <h3 className="h5 py-3">Daños</h3>
-                    <p>
+                  <div className="bg-white shadow-xs rounded-xl p-4 mb-4 mb-2-xs bg-success-soft-hover transition-bg-ease-150 text-decoration-none text-gray-800 h-100">
+                    <div className="text-center p-0 m-0">
+                      <i className="fi fi-round-info-full fs--45"></i>
+                    </div>
+                    <h3 className="h5 py-3 text-center">Daños</h3>
+                    <p className="text-justify">
                       Este plan no cubre daños en la tarjeta principal, módulos
                       de pantalla, daños por humedad, además de esto no se
                       repara ni se cambia baterías, memorias, cargadores, etc.
@@ -81,18 +96,6 @@ const Recoveries = () => {
             </div>
           </div>
         </div>
-        <svg
-          class="absolute-full z-index-0"
-          width="100%"
-          height="100%"
-          viewBox="0 0 1920 90"
-          preserveAspectRatio="none"
-        >
-          <path
-            fill="rgba(0,0,0,.01)"
-            d="M1920,0C1217,0,120.574,155.567,0,0v90h1920V0z"
-          ></path>
-        </svg>
       </section>
     </>
   );

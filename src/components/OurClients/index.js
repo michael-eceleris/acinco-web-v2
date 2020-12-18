76dc44bbc91161ad760d4301194f87c4675c8ad9
michael-eceleris <michael@eceleris.com>
2020-12-17 19:05:59 -0500

    page-home-btn

--- src/components/OurClients/index.js
+++ src/components/OurClients/index.js
@@ -11,6 +11,9 @@ import LogoAxaService from "../../assets/icons/axa-assistance.svg";
 const OurClients = () => {
   return (
     <section className="bg-theme-color-light">
+      <div className="mx-auto max-w-600">
+        <h2 className="h3-xs text-center">Nuestros clientes</h2>
+      </div>
       <div className="container z-index-1">
         <div className="mt-5 p-3 bg-white shadow-md rounded overflow-hidden">
           <div className="bg-white overflow-hidden">
@@ -18,7 +21,7 @@ const OurClients = () => {
               <div className="h--150 col-6 col-md-3 d-flex align-items-center text-center">
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-80 img-fluid opacity-7 ml-3 mr-3  lazy"
+                    className="max-h-80 img-fluid ml-3 mr-3  lazy"
                     src={LogoHuawei}
                     alt="logo_huawei"
                   />
@@ -33,7 +36,7 @@ const OurClients = () => {
               >
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-600 img-fluid opacity-7 ml-3 mr-3 max-w-180 lazy"
+                    className="max-h-600 img-fluid ml-3 mr-3 max-w-180 lazy"
                     src={LogoSamgung}
                     alt="logo_samsung"
                   />
@@ -48,7 +51,7 @@ const OurClients = () => {
               >
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-700 img-fluid opacity-7 ml-3 mr-3 max-w-180 lazy"
+                    className="max-h-700 img-fluid ml-3 mr-3 max-w-180 lazy"
                     data-src="demo.files/svg/vendors/vendor_coinbase.svg"
                     src={LogoAxaColpatria}
                     alt="logo_axa_colpatria"
@@ -64,7 +67,7 @@ const OurClients = () => {
               >
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-80 img-fluid opacity-7 ml-3 mr-3 max-w-120 lazy"
+                    className="max-h-80 img-fluid ml-3 mr-3 max-w-120 lazy"
                     data-src="demo.files/svg/vendors/vendor_instagram.svg"
                     src={LogoTigo}
                     alt="logo_tigo"
@@ -80,7 +83,7 @@ const OurClients = () => {
               >
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-120 img-fluid opacity-7 ml-3 mr-3 max-w-180 lazy"
+                    className="max-h-120 img-fluid ml-3 mr-3 max-w-180 lazy"
                     data-src="demo.files/svg/vendors/vendor_pinterest.svg"
                     src={LogoAxaService}
                     alt="logo_axa_service"
@@ -96,7 +99,7 @@ const OurClients = () => {
               >
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-600 img-fluid opacity-7 ml-3 mr-3 max-w-180 lazy"
+                    className="max-h-600 img-fluid ml-3 mr-3 max-w-180 lazy"
                     data-src="demo.files/svg/vendors/vendor_dribble.svg"
                     src={LogoTelefonicaTelecom}
                     alt="logo_telefonica_telecom"
@@ -112,7 +115,7 @@ const OurClients = () => {
               >
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-600 img-fluid opacity-7 ml-3 mr-3 max-w-180 lazy"
+                    className="max-h-600 img-fluid ml-3 mr-3 max-w-180 lazy"
                     src={LogoTelefonicaData}
                     alt="logo_telefonica_data"
                   />
@@ -127,7 +130,7 @@ const OurClients = () => {
               >
                 <a href="#!" className="w-100">
                   <img
-                    className="max-h-600 img-fluid opacity-7 ml-3 mr-3 max-w-180 lazy"
+                    className="max-h-600 img-fluid ml-3 mr-3 max-w-180 lazy"
                     src={LogoEricsson}
                     alt="logo_ericsson"
                   />

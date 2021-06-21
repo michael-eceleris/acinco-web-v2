ef5f685178b698fa5d43d656d437a0d6f1400544
michael-eceleris <michael@eceleris.com>
2021-06-21 14:57:04 -0500

    feat: add section recoveries and exclutions motorola page

--- src/pages/Motorola.js
+++ src/pages/Motorola.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 import { Helmet } from "react-helmet";
 import Banner from "../layout/layoutMotorola/Banner";
 import Footer from "../components/Footer";
-import Coverages from "../layout/layoutMotorola/Coverages"
+import Coverages from "../layout/layoutMotorola/Coverages";
+import Recoveries from "../layout/layoutMotorola/RecoveriesAndExclutions";
 
 const Motorola = () => {
-  return ( 
-    <> 
+  return (
+    <>
       <Helmet>
         <title>Motorola | Acinco</title>
-        <meta 
-          name="description"
-          content="Este seguro va dirigido a los usuarios que compren su equipo + seguro o asistencia por medio de la pagina web de Motorola."
+        <meta
+          name='description'
+          content='Este seguro va dirigido a los usuarios que compren su equipo + seguro o asistencia por medio de la pagina web de Motorola.'
         />
       </Helmet>
       <Banner />
       <Coverages />
+      <Recoveries />
       <Footer />
     </>
-   );
-}
- 
+  );
+};
+
 export default Motorola;

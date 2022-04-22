2345ae486315fac07768442579cc3e96f21ad45d
michael-eceleris <michael@eceleris.com>
2022-04-22 16:31:42 -0500

    fix(): change ccb banner

--- src/layout/layoutCamaraComercio/Banner.js
+++ src/layout/layoutCamaraComercio/Banner.js
@@ -21,7 +21,7 @@ const Banner = () => {
       <div className='container min-h75vh d-middle pt-5'>
         <div className='row text-center-xs'>
           <div
-            className='col-12 col-md-6 order-2 order-md-1 pb--150 pl-md-3'
+            className='col-12 col-md-6 order-2 order-md-1 pb--180 pl-md-3'
             data-aos='fade-in'
             data-aos-delay='0'
           >
@@ -56,7 +56,7 @@ const Banner = () => {
           </div>
           <div className='col-12 col-md-6 order-2 order-md-1 d-middle show2'>
             <img
-              width='270'
+              width='300'
               className='img-fluid lazy '
               src={PhoneRepair}
               alt='logo_camara_comercio'
